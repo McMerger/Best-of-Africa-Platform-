3fa772766dc014f1dd52a3209b9e7795ad32132b
Synergy <synergy@bestofafrica.com>
2026-04-27 13:54:01 -0400

    Remove test migration artifact 9999_test_task.sql

--- migrations/9999_test_task.sql
+++
@@ -1,5 +0,0 @@
--- This file has been neutralized. Test fixture moved to fixtures/test_task_seed.sql.
--- Delete this file before running db:migrate:prod.
--- Apply test data locally only with:
---   wrangler d1 execute best-of-africa-db --local --file=fixtures/test_task_seed.sql
-SELECT 1;
